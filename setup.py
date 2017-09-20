bf7bb15e33bcc179d8683869c84dba7e5c21ba25
Eli Draizen <edraizen@gmail.com>
2017-09-20 15:12:56 -0400

    Make installable

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'unet3d',
+      description = 'Keras 3D U-Net Convolution Neural Network (CNN) designed for medical image segmentation',
+      author = 'David G Ellis',
+      license = 'MIT',
+      packages = ['unet3d'])
