40882cbf9bb950f58cbfe7b3bdeb8e542b411575
Eli Jacob Draizen <draizene@biowulf.nih.gov>
2017-10-03 14:49:53 -0400

    removed extra kwd

--- spheres/train.py
+++ spheres/train.py
@@ -9,7 +9,7 @@ K.set_image_dim_ordering('tf')
 input_shape=(96,96,96,1)
 model = unet_model_3d(input_shape=input_shape)
 
-train_generator, validation_generator = ExampleSphereGenerator.get_training_and_validation(input_shape, cnt=5, border=10, cnt=5, batch_size=20, n_samples=500)
+train_generator, validation_generator = ExampleSphereGenerator.get_training_and_validation(input_shape, cnt=5, border=10, batch_size=20, n_samples=500)
 
 train_model(model=model, 
             model_file=os.path.abspath("./SphereCNN.h5"), 
